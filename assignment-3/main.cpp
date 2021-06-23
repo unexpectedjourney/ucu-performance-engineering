c9631afcf986e8cde6b24852b5e6347651cd3cec
Anton Babenko <antonwarbeck123@gmail.com>
2021-06-24 01:40:25 +0300

    Added third task

--- assignment-3/main.cpp
+++ assignment-3/main.cpp
@@ -128,6 +128,36 @@ void basic_conv(std::vector<int> &array, std::vector<int> filter, std::vector<in
   }
 }
 
+void conv_slice(std::vector<int> &array, std::vector<int> &filter, std::vector<int> &result_array, int start_pos, int end_pos) {
+  for (int i = start_pos; i < end_pos; ++i) {
+    int value = 0;
+    int start_point =  i - (filter.size() / 2);
+    for (int j = 0; j < filter.size(); ++j) {
+      int current_position = start_point + j;
+      if (current_position < 0 || current_position >= result_array.size()) {
+        continue;
+      }
+      value = value + array[current_position] * filter[j];
+    }
+    result_array[i] = value;
+  }
+}
+
+void multithread_conv(std::vector<int> &array, std::vector<int> &filter, std::vector<int> &result_array) {
+  int thread_amount = 16;
+  int block_size = N / thread_amount;
+
+  std::vector<std::thread> threads;
+  for (int i = 0; i < thread_amount; ++i) {
+    int start_pos = i * block_size;
+    int end_pos = (i + 1) * block_size;
+    threads.push_back(std::thread(conv_slice, std::ref(array), std::ref(filter), std::ref(result_array), start_pos, end_pos));
+  }
+  for (int i = 0; i < threads.size(); ++i) {
+    threads[i].join();
+  }
+}
+
 void task3() {
   std::vector<int> array(N, 0);
   random_array(array);
@@ -140,9 +170,11 @@ void task3() {
     printf("%d\t", result_array[i]);
   }
 
-//  std::atomic<int> m_result{0};
-//  multithread_min(array, m_result);
-//  printf("%d\n", int(m_result));
+  std::vector<int> m_result_array(N, 0);
+  multithread_conv(array, filter, m_result_array);
+  for (int i = 0; i < m_result_array.size(); ++i) {
+    printf("%d\t", m_result_array[i]);
+  }
 }
 
 
